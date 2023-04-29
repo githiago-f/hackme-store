e04c641832518da9be1a58588075cb4ca9c0b38c
githiago-f <tfarias@protonmail.com>
2023-04-29 11:10:15 -0400

    update start-server bash script so it dont ask for sudo after it is granted the capability to bind net ports

--- start-server.sh
+++ start-server.sh
@@ -1,20 +1,39 @@
+#!/bin/bash
+################################################################################
+#                                  README                                      #
+################################################################################
+# This script should be inserted on the .bashrc file after "nvm"               #
+# initialization.                                                              #
+################################################################################
+cd ~/projects/hackme-store;
+
 # verify if git exists first
 if command -v git &> /dev/null; then
   git pull origin master;
 fi
 
 # gets local machine address
-ADDR=`ifconfig enp0s3 | grep -Eom1 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d " " -f2`
+ADDR=`ifconfig enp0s3 | grep -Eom1 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d " " -f2`;
 
 # set default port to default http port
 export PORT=80;
-# allow nodejs to use http port:
-sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\`` > /dev/null;
+
+# check if already set the file capabilities with setcap
+HAS_CAPABILITY=$(getcap `readlink -f \`which node\`` | grep -Eom1 "cap_net_\w*");
+if ! [[ "$HAS_CAPABILITY" == "cap_net_bind_service" ]]; then
+  echo "Insert the root password to set capability \"cap_net_bind_service\" to node:";
+  # allow nodejs to use net bind:
+  sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\`` > /dev/null;
+else
+  echo "Already has capability to use port 80...";
+fi
 
 # if npm exists, execute the program
 if command -v npm &> /dev/null; then
-  npm run migrate;
-  npm run seed;
+  npm run migrate &> /dev/null;
+  npm run seed &> /dev/null;
   export ADDR=$ADDR;
-  npm run start;
+  npm run start &> logs.log &
 fi
+
+cd ~;
